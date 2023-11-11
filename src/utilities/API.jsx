dac0c0d1544dc81bceb92a6f605bc043619e4d98
Tyler <t.woods95@aol.com>
2023-11-11 15:47:46 -0500

    updated api imported axios get search method and exported

--- src/utilities/API.jsx
+++ src/utilities/API.jsx
@@ -1,5 +1,8 @@
-// make a search to PokeAPI
+// Import Axios
+import axios from 'axios';
 
-// export const searchOMDB = (query) => {
-//     return fetch(`https://www.omdbapi.com/?t=${query}&apikey=9f162971`);
-//   };
+// Using axios, we create a search method that is specific to our use case and export it at the bottom
+const search = (query) => axios.get(`https://pokeapi.co/api/v2/pokemon-species/{name}/&limit=10`);
+
+// Export an object with a "search" method that searches the PokeAPI for the passed query
+export default search;

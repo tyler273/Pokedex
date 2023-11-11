b821a6be87b47dd5e2110e6f475d766cda3cb81e
Tyler <t.woods95@aol.com>
2023-11-11 16:11:39 -0500

    Updated axios get removed limit

--- src/utilities/API.jsx
+++ src/utilities/API.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Using axios, we create a search method that is specific to our use case and export it at the bottom
-const search = (query) => axios.get(`https://pokeapi.co/api/v2/pokemon-species/{name}/&limit=10`);
+const search = (query) => axios.get(`https://pokeapi.co/api/v2/pokemon-species/{name}/`);
 
 // Export an object with a "search" method that searches the PokeAPI for the passed query
 export default search;
